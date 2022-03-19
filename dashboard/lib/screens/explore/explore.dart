3003fcf5c90ef2d5be618dd858390f526cb4d3eb
Michael Lichtenecker <michael.lichtenecker@gmail.com>
2022-03-19 23:15:33 +0100

    worked on explorer in dashboard

--- dashboard/lib/screens/explore/explore.dart
+++ dashboard/lib/screens/explore/explore.dart
@@ -2,6 +2,7 @@ import 'package:dashboard/controllers/timeline_controller.dart';
 import 'package:dashboard/utils/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:dashboard/screens/overlay/overlay.dart' as topOverlay;
+import 'package:flutter_expandable_table/flutter_expandable_table.dart';
 import 'package:provider/provider.dart';
 
 import '../../controllers/app_controller.dart';
@@ -31,9 +32,59 @@ class _ExploreState extends State<Explore> {
         child: Container(
             height: size.height - topbarHeight,
             width: size.width - sidebarWidth,
-            color: Colors.red,
-            child: Table(
-              children: [],
-            )));
+            child: ExpandableTable(
+                rows: rows,
+                header: header,
+                scrollShadowColor: Theme.of(context).focusColor)));
   }
 }
+
+const Color primaryColor = Color(0xFF1e2f36); //corner
+const Color accentColor = Color(0xFF0d2026); //background
+const TextStyle textStyle = TextStyle(color: Colors.white);
+const TextStyle textStyleSubItems = TextStyle(color: Colors.grey);
+
+List<String> headers = ["application_id", "analytic", "event", "crash"];
+
+List<Widget> gen() {
+  List<Widget> a = [];
+  for (var x in headers) {
+    var y = Container(child: Center(child: Text(x)));
+    a.add(y);
+  }
+  return a;
+}
+
+ExpandableTableHeader header = ExpandableTableHeader(
+    firstCell: Container(
+        color: primaryColor,
+        child: Center(
+            child: Text(
+          'session_id',
+          style: textStyle,
+        ))),
+    children: gen());
+//Creation rows
+List<ExpandableTableRow> rows = List.generate(
+    headers.length,
+    (rowIndex) => ExpandableTableRow(
+          height: 50,
+          firstCell: Container(
+              color: primaryColor,
+              margin: EdgeInsets.all(1),
+              child: Center(
+                  child: Text(
+                'Row $rowIndex',
+                style: textStyle,
+              ))),
+          children: List<Widget>.generate(
+              headers.length,
+              (columnIndex) => Container(
+                  color: primaryColor,
+                  margin: EdgeInsets.all(1),
+                  child: Center(
+                      child: Text(
+                    'Cell $rowIndex:$columnIndex',
+                    style: textStyle,
+                  )))),
+        ));
--- dashboard/lib/screens/overlay/topbar/logo_container.dart
+++ dashboard/lib/screens/overlay/topbar/logo_container.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:routemaster/routemaster.dart';
 
 class LogoContainer extends StatelessWidget {
   const LogoContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.only(left: 20),
-        width: 175,
-        height: 50,
-        child: Image.asset("img/opensight.png"));
+    return GestureDetector(
+        onTap: () {
+          Routemaster.of(context).push('/');
+        },
+        child: Container(
+            margin: const EdgeInsets.only(left: 20),
+            width: 175,
+            height: 50,
+            child: Image.asset("img/opensight.png")));
   }
 }
