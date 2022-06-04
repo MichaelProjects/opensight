017f5ec7dc658d8449a7895ae27cf78b085f7788
MichaelProjects <michael.lichtenecker@outlook.com>
2022-06-04 20:44:24 +0200

    worked on bake .env

--- dashboard/scripts/bake_env.sh
+++ dashboard/scripts/bake_env.sh
@@ -1,4 +1,11 @@
-env_path="/usr/share/nginx/html/assets/assets/.env"
+#!/bin/bash
+
+#env_path="/usr/share/nginx/html/assets/assets/.env"
+env_path="/Users/michael/Documents/Analytics/dashboard/.env"
+rm $env_path
 touch $env_path
-echo $1
-echo $1 >> $env_path
+
+if [ -z "$OPENSIGHT_CORE_URL" ];
+  then echo "$OPENSIGHT_CORE_URL is null";
+  else echo "OPENSIGHT_CORE_URL=$OPENSIGHT_CORE_URL" >> $env_path;
+fi
